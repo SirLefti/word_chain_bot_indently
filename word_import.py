d966be5cc759048a76c5a02e20119332ae3a2941
SirLefti <62506842+SirLefti@users.noreply.github.com>
2025-12-09 20:02:00 +0100

    word importer for wordschatz datasets in case we need it

---
+++ word_import.py
@@ -0,0 +1,38 @@
+import argparse
+import asyncio
+import re
+
+from sqlalchemy.sql.expression import insert
+
+from character_frequency import __CACHE_DIRECTORY, __LANGUAGE_SOURCES
+from language import Language
+from main import word_chain_bot
+from model import WordCacheModel
+from wortschatz import extract_words
+
+
+async def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--language', type=str, help='ISO-639-1 language code', required=True)
+
+    args = parser.parse_args()
+    language = Language.from_language_code(args.language)
+
+    words = await extract_words(__LANGUAGE_SOURCES[language], __CACHE_DIRECTORY)
+    regex = re.compile(language.value.allowed_word_regex)
+    accepted_words = [word for word in words if regex.match(word)]
+    total_words = len(accepted_words)
+
+    async with word_chain_bot.db_connection(locked=True) as connection:
+        for index, word in enumerate(accepted_words):
+            statement = insert(WordCacheModel).values(
+                word=word,
+                language=language.value.code
+            ).prefix_with('OR IGNORE')
+            await connection.execute(statement)
+            if index % 100 == 0:
+                print(f'({(index/total_words):.2%}) {index}/{total_words}')
+        await connection.commit()
+
+if __name__ == '__main__':
+    asyncio.run(main())
